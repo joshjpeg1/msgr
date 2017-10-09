71fd1f51ef88a80c398b27f37f8c842c40af46ee
joshjpeg1 <joshuapensky@gmail.com>
2017-10-08 21:25:43 -0400

    Fixed bug with year dates, refactored main function into helpers

--- lib/msgr/messages/dates.ex
+++ lib/msgr/messages/dates.ex
@@ -2,48 +2,57 @@ defmodule Msgr.Dates do
 	def get_msg_time(msg, curr_day, curr_time) do
 		curr_utc = calc_utc(curr_time.hour, curr_time.minute, curr_time.second)
 		msg_utc =  calc_utc(msg.hour, msg.minute, msg.second)
-		sd_time_dif = curr_utc - msg_utc
-		nd_time_dif = (86400 - msg_utc) + curr_utc
+		sd_time_dif = curr_utc - msg_utc  # same day time difference
+		nd_time_dif = (86400 - msg_utc) + curr_utc  # next day time difference
 		
 		same_month = (curr_day.month == msg.month) and ((curr_day.day < msg.day) or (curr_day.day == msg.day and (sd_time_dif < 0)))
 
-		if (curr_day.year == msg.year) or ((curr_day.year - msg.year == 1) and (same_month or (curr_day.month < msg.month))) do
-
-			next_day = ((curr_day.month == msg.month) and (curr_day.day - msg.day == 1))
-			next_day = next_day or ((curr_day.month - msg.month == 1) and (curr_day.day - msg.day == 1))
-			next_day = next_day or (curr_day.month == 1 and msg.month == 12 and curr_day.day == 1 and msg.day == Date.days_in_month(msg))
-			
-			same_day = (curr_day.month == msg.month) and (curr_day.day == msg.day)			
-			
-			# less than a day
-			if ((sd_time_dif > 0) and same_day) or ((sd_time_dif < 0) and next_day) do
-				# less than an hour
-				if ((sd_time_dif < 3600) and same_day) or ((nd_time_dif < 3600) and next_day) do
-					# less than a minute
-					if ((sd_time_dif < 60) and same_day) or ((nd_time_dif < 60) and next_day) do
-						"Just now"
-					else
-						date_in_minutes(same_day, sd_time_dif, nd_time_dif)
-					end
-				else
-					date_in_hours(same_day, sd_time_dif, nd_time_dif)
-				end
-			else
-				date_over_day(msg)
-			end
+		# less than a year
+		if (curr_day.year == msg.year) or ((curr_day.year - msg.year == 1) and (same_month or (curr_day.day < msg.day))) do
+			check_less_than_day(msg, curr_day, curr_time, sd_time_dif, nd_time_dif)
 		else
 			date_over_year(msg)
 		end
 	end
 
-	def date_in_minutes(same_day, sd_time_dif, nd_time_dif) do
-		if same_day do
-			"#{round(sd_time_dif / 60)}m"
+	def check_less_than_day(msg, curr_day, curr_time, sd_time_dif, nd_time_dif) do
+		next_day = ((curr_day.month == msg.month) and (curr_day.day - msg.day == 1))
+    next_day = next_day or ((curr_day.month - msg.month == 1) and (curr_day.day - msg.day == 1))
+    next_day = next_day or (curr_day.month == 1 and msg.month == 12 and curr_day.day == 1 and msg.day == Date.days_in_month(msg))
+
+    same_day = (curr_day.month == msg.month) and (curr_day.day == msg.day)
+
+		if ((sd_time_dif > 0) and same_day) or ((sd_time_dif < 0) and next_day) do
+			check_less_than_hour(same_day, next_day, sd_time_dif, nd_time_dif)
 		else
-			"#{round(nd_time_dif / 60)}m"
+			date_over_day(msg)
 		end
 	end
 
+	def check_less_than_hour(same_day, next_day, sd_time_dif, nd_time_dif) do
+		if ((sd_time_dif < 3600) and same_day) or ((nd_time_dif < 3600) and next_day) do
+			check_less_than_minute(same_day, next_day, sd_time_dif, nd_time_dif)
+		else
+			date_in_hours(same_day, sd_time_dif, nd_time_dif)
+		end
+	end
+
+	def check_less_than_minute(same_day, next_day, sd_time_dif, nd_time_dif) do
+		if ((sd_time_dif < 60) and same_day) or ((nd_time_dif < 60) and next_day) do
+			"Just now"
+		else
+			date_in_minutes(same_day, sd_time_dif, nd_time_dif)
+		end
+	end
+
+	def date_over_year(msg) do
+		"#{msg.day} #{get_month_short(msg.month)} #{msg.year}"
+	end
+
+	def date_over_day(msg) do
+		"#{get_month_short(msg.month)} #{msg.day}"
+	end
+	
 	def date_in_hours(same_day, sd_time_dif, nd_time_dif) do
 		if same_day do
       "#{round(sd_time_dif / 3600)}h"
@@ -51,13 +60,13 @@ defmodule Msgr.Dates do
     	"#{round(nd_time_dif / 3600)}h"
 		end
 	end
-
-	def date_over_day(msg) do
-		"#{get_month_short(msg.month)} #{msg.day}"
-	end
-
-	def date_over_year(msg) do
-		"#{msg.day} #{get_month_short(msg.month)} #{msg.year}"
+	
+	def date_in_minutes(same_day, sd_time_dif, nd_time_dif) do
+		if same_day do
+			"#{round(sd_time_dif / 60)}m"
+		else
+			"#{round(nd_time_dif / 60)}m"
+		end
 	end
 
 	def get_month_short(month) do
